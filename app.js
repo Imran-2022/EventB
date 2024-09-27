04d9d1597f2cf3a953b6f5c9845a19defa46e603
Imran-2022 <mdimranulhaque202@gmail.com>
2024-09-27 10:25:13 +0600

    event back start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors=require('cors');
+// cross origin resource sharing.
+
+const app =express();
+
+app.use(cors());
+app.use(express.json());
+
+module.exports=app;
