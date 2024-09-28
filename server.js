cdd97999a2f1912025cd4e9947191f00e9ab83d8
Imran-2022 <mdimranulhaque202@gmail.com>
2024-09-28 10:16:41 +0600

    server errr

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_SERVER)
 });
 
 
-const port =process.env.PORT || 3001;
+const port =process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
